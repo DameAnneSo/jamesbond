e49a5c2c0e6845f2005d5d0d2d2cf7cd7585cb47
Anne-Sophie Pereira De Sa <annpereiradesa@linkedin.com>
2024-05-15 13:49:46 +0100

    session 1: adding a svg with svelte

--- src/components/session1.svelte
+++ src/components/session1.svelte
@@ -1,17 +1,31 @@
+<script>
+  const data = [
+    { key: 30, value: 20, category: 'typeA' },
+    { key: 60, value: 130, category: 'typeB' },
+    { key: 90, value: 50, category: 'typeA' },
+    { key: 120, value: 100, category: 'typeB' },
+    { key: 150, value: 30, category: 'typeB' },
+    { key: 180, value: 120, category: 'typeC' },
+    { key: 210, value: 70, category: 'typeC' },
+    { key: 240, value: 50, category: 'typeC' },
+    { key: 270, value: 110, category: 'typeC' },
+    { key: 250, value: 20, category: 'typeB' }
+  ]
+
+  const colourDictionary = {
+    typeA: 'blue',
+    typeB: 'red',
+    typeC: 'green'
+  }
+</script>
+
 <h1>Scatterplot in svg</h1>
-<p>ReminderLI have questions about the starter pack</p>
+
 <svg width="60ch" viewbox="0 0 300 150">
   <rect x="0" y="0" height="100%" width="100%" fill="teal" opacity="0.2" />
 
   <!-- Random circles of radius 5 -->
-  <circle cx="30" cy="20" r="5" fill="blue" />
-  <circle cx="60" cy="130" r="5" fill="red" />
-  <circle cx="90" cy="50" r="5" fill="blue" />
-  <circle cx="120" cy="100" r="5" fill="red" />
-  <circle cx="150" cy="30" r="5" fill="red" />
-  <circle cx="180" cy="120" r="5" fill="green" />
-  <circle cx="210" cy="70" r="5" fill="green" />
-  <circle cx="240" cy="50" r="5" fill="green" />
-  <circle cx="270" cy="110" r="5" fill="green" />
-  <circle cx="250" cy="20" r="5" fill="red" />
+  {#each data as datum}
+    <circle cx={datum.key} cy={datum.value} r="5" fill={colourDictionary[datum.category]} />
+  {/each}
 </svg>
