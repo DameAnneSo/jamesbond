4a03b64e5e26596df94326044160ceb2308ffa98
Anne-Sophie Pereira De Sa <annpereiradesa@linkedin.com>
2024-05-31 15:43:59 +0100

    adding interaction

--- src/App.svelte
+++ src/App.svelte
@@ -3,13 +3,28 @@
   import D3Pure from './components/chart_d3only.svelte'
   import D3Svelte from './components/chart_svelte.svelte'
 
+function opacityClick() {
+if (opacity ===1)
+{opacity = 0.5}
+else 
+{opacity = 1}
+}
+
+let opacity = 1 
+$: console.log(opacity)
+
+
 </script>
 
 <div class="page-column">
+  <!-- < class="page-column" style:opacity={opacity}> -->
   <!-- <h1>Charts avec svg, D3 et Svelte</h1> -->
   <HardcodedSVG />
   <D3Pure />
-  <D3Svelte />
+  <D3Svelte opacityChange={opacity}/>
+
+  <button on:click={opacityClick}>click me</button>
+
 </div>
 
 
--- src/components/chart_svelte.svelte
+++ src/components/chart_svelte.svelte
@@ -11,6 +11,8 @@ import dataRaw from '../data/lineData.csv'
 import { onMount } from 'svelte'
 onMount(() => {draw()})
 
+export let opacityChange = 1
+
 // Data processing
 const lineData = dataRaw.map(d => ({
     date: utcParse('%d/%m/%Y')(d.date),
@@ -52,19 +54,17 @@ const y = scaleLinear(
 // Draw the chart
 const draw = () => {
 // Add the x-axis
-select("#d3_Svelte_chart")
- .append('g')
+select("g.axisX")
  .attr('transform', `translate(0,${height - marginBottom})`)
  .call(
-   axisBottom(x)
-   .ticks(width / 80) // I dont understand how this works
-   .tickFormat(utcFormat('%b'))
+  axisBottom(x)
+  .ticks(12) // I dont understand how this works
+  .tickFormat(utcFormat('%b'))
     //  .tickSizeOuter(0)
  )
 
 // Add the y-axis, remove the domain line, add grid lines and a label.
-select("#d3_Svelte_chart")
- .append('g')
+select("g.axisY")
  .attr('transform', `translate(${marginLeft},0)`)
  .call(axisLeft(y).ticks(height / 40))
  .call(g => g.select('.domain').remove())
@@ -85,15 +85,16 @@ select("#d3_Svelte_chart")
 </script>
 
 <!-- new svg in d3 and Svelte -->
-<!-- <h2>Scatterplot built with d3 and Svelte</h2> -->
+<h2>Scatterplot built with d3 and Svelte</h2>
 <svg id='d3_Svelte_chart' width="60ch" viewbox="0 0 500 300">
   <rect x="0" y="0" height="100%" width="100%" fill="purple" opacity="0.2" />
+  <g class="axisX"></g>
+  <g class="axisY"></g>
   <!-- add marks -->
+  <g style:opacity={opacityChange}>
   {#each lineData as datum}
-    <circle cx={x(datum.date)} cy={y(datum.level)} r="5" fill={colourDictionary[datum.type]} />
+    <circle cx={x(datum.date)} cy={y(datum.level)} r="5" fill={colourDictionary[datum.type]}/>
   {/each}
+  </g>
 </svg>
 
-<!-- ajuster axe, comment changer le format - regarder scaleUTC and format pour formatter la date 
-DONE BUT Question on row 60
-also, the axes are above the chart and not the other way round?-->
